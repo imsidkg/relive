e3abcd2cd66c33a2414d077fe3dfcb524e1299e2
Siddharth Kumar Gope <siddharth8271skg2@gmail.com>
2025-10-17 18:19:22 +0530

    read tool agent tool

--- backend/src/inngest/simple-agent.ts
+++ backend/src/inngest/simple-agent.ts
@@ -1,4 +1,4 @@
-import { createAgent, createTool, gemini } from "@inngest/agent-kit";
+import { createAgent, createTool, gemini, Network } from "@inngest/agent-kit";
 import z from "zod";
 import { getSandbox } from "../lib/utils";
 
@@ -18,9 +18,39 @@ const listFiles = createTool({
   },
 });
 
+const writeFiles = createTool({
+  name: "writeFiles",
+  description: "Create or update files in the sandbox",
+  parameters: z.object({ path: z.string(), content: z.string() }),
+  handler: async (parameters, { network }) => {
+    const sandbox = await getSandbox(network);
+    const files = await sandbox.files.write(
+      parameters.path,
+      parameters.content
+    );
+    return files;
+  },
+});
+
+const readFiles = createTool({
+  name: "readFiles",
+  description: "Reads file's content according to the path provided",
+  parameters: z.object({ path: z.string() }),
+  handler: async (parameters, { network }) => {
+    try {
+      const sandbox = await getSandbox(network);
+      const content = await sandbox.files.read(parameters.path);
+      return content;
+    } catch (error) {
+      console.error("Failed to fetch file content");
+      return;
+    }
+  },
+});
+
 export const simpleAgent = createAgent({
   name: "SImple tool agent",
-  tools: [listFiles],
+  tools: [listFiles, writeFiles , readFiles],
   system: "An expert coding agent",
   model: gemini({
     model: "gemini-2.5-pro",
